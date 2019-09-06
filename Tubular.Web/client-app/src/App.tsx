cab87df523e18ff36b6cecdd2a8c7beef8668806
JCorrea13 <josemanuelcorrea.13@gmail.com>
2019-09-06 15:50:04 -0500

    workshop

--- Tubular.Web/client-app/src/App.tsx
+++ Tubular.Web/client-app/src/App.tsx
@@ -1,15 +1,42 @@
 import * as React from 'react';
-import {DataGrid} from 'tubular-react';
-import {ColumnsDefinition} from './ColumnsDefinition';
+import { DataGrid, ToolbarOptions } from 'tubular-react';
+import { ColumnsDefinition } from './ColumnsDefinition';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import Checkbox from '@material-ui/icons/CheckBox';
+import CheckBoxOutlineBlank from '@material-ui/icons/CheckBoxOutlineBlank';
+
+const onClick = () => alert('Hello Tubular');
+
+const customToolBar = new ToolbarOptions({
+    advancePagination: false,
+    exportButton: false,
+    printButton: false,
+    rowsPerPageOptions: [1, 3, 5, 7, 9],
+    customItems: (<button onClick={onClick}> Click Me </button>)
+});
 
 const App: React.FunctionComponent = () => {
+
+
+    const customBody = (row: any) =>
+        (<TableRow key={row.OrderId}>
+            <TableCell>{row.CustomerName}</TableCell>
+            <TableCell>{row.ShippedDate}</TableCell>
+            <TableCell>{row.ShipperCity}</TableCell>
+            <TableCell>{row.IsShipped ? <Checkbox />: <CheckBoxOutlineBlank />}</TableCell>
+            <TableCell>{row.Amount}</TableCell>
+        </TableRow>)
+
     return (
         <>
             <h2>Tubular</h2>
             <DataGrid
                 gridName='Orders'
+                toolbarOptions={customToolBar}
                 columns={ColumnsDefinition}
                 dataSource={'http://localhost:51244/api/tubular/paged'}
+                bodyRenderer={customBody}
             />
         </>
     );
--- Tubular.Web/client-app/src/ColumnsDefinition.ts
+++ Tubular.Web/client-app/src/ColumnsDefinition.ts
@@ -1,11 +1,41 @@
 
-import { ColumnModel } from 'tubular-common';
+import { AggregateFunctions, ColumnDataType, ColumnModel, ColumnSortDirection } from 'tubular-common';
 
 export const ColumnsDefinition = [
-    new ColumnModel('OrderID'),
-    new ColumnModel('CustomerName'),
-    new ColumnModel('ShippedDate'),
-    new ColumnModel('ShipperCity'),
-    new ColumnModel('IsShipped'),
-    new ColumnModel('Amount'),
-  ];
+  new ColumnModel('OrderID',
+    {
+      IsKey: true,
+      Visible: false,
+    }),
+  new ColumnModel('CustomerName',
+    {
+      Aggregate: AggregateFunctions.COUNT,
+      Filterable: true,
+      Searchable: true,
+      Sortable: true,
+    }),
+  new ColumnModel('ShippedDate',
+    {
+      DataType: ColumnDataType.DATE_TIME,
+      Filterable: false,
+      SortDirection: ColumnSortDirection.ASCENDING,
+      Sortable: true,
+    }),
+  new ColumnModel('ShipperCity',
+    {
+      Filterable: true,
+      Searchable: true,
+    }),
+  new ColumnModel('IsShipped',
+    {
+      DataType: ColumnDataType.BOOLEAN,
+      Filterable: true,
+      Searchable: false,
+    }),
+  new ColumnModel('Amount',
+    {
+      Filterable: true,
+      Label: 'Price',
+      Sortable: true,
+    }),
+];
