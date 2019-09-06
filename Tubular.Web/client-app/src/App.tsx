c190ca82977a74b1736141156463394966f1f8eb
JCorrea13 <josemanuelcorrea.13@gmail.com>
2019-09-06 15:58:49 -0500

    code refactor

--- Tubular.Web/client-app/src/App.tsx
+++ Tubular.Web/client-app/src/App.tsx
@@ -1,34 +1,34 @@
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import Checkbox from '@material-ui/icons/CheckBox';
+import CheckBoxOutlineBlank from '@material-ui/icons/CheckBoxOutlineBlank';
 import * as React from 'react';
 import { DataGrid, ToolbarOptions } from 'tubular-react';
 import { ColumnsDefinition } from './ColumnsDefinition';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import Checkbox from '@material-ui/icons/CheckBox';
-import CheckBoxOutlineBlank from '@material-ui/icons/CheckBoxOutlineBlank';
 
 const onClick = () => alert('Hello Tubular');
 
 const customToolBar = new ToolbarOptions({
     advancePagination: false,
+    customItems: (<button onClick={onClick}> Click Me </button>),
     exportButton: false,
     printButton: false,
     rowsPerPageOptions: [1, 3, 5, 7, 9],
-    customItems: (<button onClick={onClick}> Click Me </button>)
 });
 
-const App: React.FunctionComponent = () => {
-
-
-    const customBody = (row: any) =>
-        (<TableRow key={row.OrderId}>
+const customBody = (row: any) =>
+    (
+        <TableRow key={row.OrderId}>
             <TableCell>{row.CustomerName}</TableCell>
             <TableCell>{row.ShippedDate}</TableCell>
             <TableCell>{row.ShipperCity}</TableCell>
-            <TableCell>{row.IsShipped ? <Checkbox />: <CheckBoxOutlineBlank />}</TableCell>
+            <TableCell>{row.IsShipped ? <Checkbox /> : <CheckBoxOutlineBlank />}</TableCell>
             <TableCell>{row.Amount}</TableCell>
-        </TableRow>)
+        </TableRow>
+    );
 
-    return (
+const App: React.FunctionComponent = () =>
+    (
         <>
             <h2>Tubular</h2>
             <DataGrid
@@ -40,6 +40,5 @@ const App: React.FunctionComponent = () => {
             />
         </>
     );
-};
 
 export default App;
